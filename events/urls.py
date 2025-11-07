ad23c1e103ee30e06d76e170917e7f0c594f6176
Luiz Otávio Lomeu <luizotavio074@gmail.com>
2025-11-07 13:04:35 -0300

    Criadas outras páginas. Separação dos partials head e body. Início do CSS.

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from events.views import contato, home
+from events.views import buscar, comofunciona, contato, fotografos, home
 
 urlpatterns = [
     path('', home),
-    path('contato/', contato)
+    path('contato/', contato),
+    path('fotografos/', fotografos),
+    path('como-funciona/', comofunciona),
+    path('buscar/', buscar),
+    #path('temp/', temp),
 ]
--- events/views.py
+++ events/views.py
@@ -4,7 +4,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'events/home.html')
+    return render(request, 'events/pages/home.html')
 
 def contato(request):
-    return render(request, 'events/contato.html')
+    return render(request, 'events/pages/contato.html')
+
+def fotografos(request):
+    return render(request, 'events/pages/fotografos.html')
+
+def comofunciona(request):
+    return render(request, 'events/pages/comofunciona.html')
+
+def buscar(request):
+    return render(request, 'events/pages/buscar.html')
+
+#def temp(request):
+#   return render(request, 'me-apague/temp.html')
